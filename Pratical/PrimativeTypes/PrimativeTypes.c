307814b85c48478bb447d55de27e2326ebfe6725
AndrewC55 <andrew.clark@solarwinds.com>
2021-03-23 20:14:59 +0000

    Added comments to PrimativeTypes

--- Pratical/PrimativeTypes/PrimativeTypes.c
+++ Pratical/PrimativeTypes/PrimativeTypes.c
@@ -2,16 +2,32 @@
 
 int main(int argc, char **argv)
 {
-    // declare variable integerVariable as type integer
+    // declaring variable integerVariable as type integer
     int integerVariable;
-    // dcelare variable size as type size_t
-    size_t size;
+    // declaring variable longVariable as type long
+    long longVariable;
+    // declaring variable shortVariable as type short
+    short shortVariable;
+    // declaring variable charVariable as type char
+    char charVariable;
+    // declaring variable floatVariable as type float
+    float floatVariable;
+    // declaring variable doubleVariable as type double
+    double doubleVariable;
 
-    // says variable size is equal to the sizeof (in memory) of integerVariable
-    size = sizeof(integerVariable);
 
-    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the variable size)
-    printf("Size of Integer Variable is %zu btyes \n", size);
+    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the size of integerVariable)
+    printf("Size of Integer Variable is %zu btyes \n", sizeof(integerVariable));
+    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the size of longVariable)
+    printf("Size of Long Variable is %zu btyes \n", sizeof(longVariable));
+    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the size of shortVariable)
+    printf("Size of Byte Variable is %zu btyes \n", sizeof(shortVariable));
+    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the size of charVariable)
+    printf("Size of Char Variable is %zu btyes \n", sizeof(charVariable));
+    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the size of floatVariable)
+    printf("Size of Float Variable is %zu btyes \n", sizeof(floatVariable));
+    // similar to php's sprintf function this line is printing the string message with an inputted specifier (%zu will be populated with the size of doubleVariable)
+    printf("Size of Double Variable is %zu btyes \n", sizeof(doubleVariable));
 
     return 0;
 }
