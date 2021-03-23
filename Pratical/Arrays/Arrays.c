dfd5c8526301c8ae9ca7bba04b8f287bcfc99159
AndrewC55 <andrew.clark@solarwinds.com>
2021-03-23 21:16:58 +0000

    Added user input for array size in Arrays.c

--- Pratical/Arrays/Arrays.c
+++ Pratical/Arrays/Arrays.c
@@ -2,34 +2,33 @@
 
 int main()
 {
-    int grades[11];
-    
-    // prints and scans so user can enter their respective grades
-    printf("Please enter grade for SD1\n");
-    scanf("%d", &grades[0]);
-    printf("Please enter grade for Info Systems\n");
-    scanf("%d", &grades[1]);
-    printf("Please enter grade for UX\n");
-    scanf("%d", &grades[2]);
-    printf("Please enter grade for Maths\n");
-    scanf("%d", &grades[3]);
-    printf("Please enter grade for Computer Systems\n");
-    scanf("%d", &grades[4]);
-    printf("Please enter grade for PP1\n");
-    scanf("%d", &grades[5]);
-    printf("Please enter grade for OOP\n");
-    scanf("%d", &grades[6]);
-    printf("Please enter grade for Database Systems\n");
-    scanf("%d", &grades[7]);
-    printf("Please enter grade for Project Management\n");
-    scanf("%d", &grades[8]);
-    printf("Please enter grade for Software Enigeering Methods\n");
-    scanf("%d", &grades[9]);
-    printf("Please enter grade for PP2\n");
-    scanf("%d", &grades[10]);
+    int matricNumber, gradeNumber;
 
-    // for loop through the grades and display
-    for (int i = 0; i < 11; i++) {
+    // ask user for matric number
+    printf("Please enter your Matric Number\n");
+    // enter matric number and assign it to the matricNumber variable
+    scanf("%d", &matricNumber);
+    // ask user for how many grades they would to enter
+    printf("Please enter the amount of grades you wish to enter\n");
+    // enter how many grades they would like to store
+    scanf("%d", &gradeNumber);
+    
+    // declare size of grades array to be whatever user has entered
+    int grades[gradeNumber];
+
+    // for loop through the amount of times user has entered
+    for (int i = 0; i < gradeNumber; ++i) {
+        // ask user for the grade of the 'x' subject
+        printf("Please enter the grade for subject number %d:\n", i+1);
+        // user enters grade
+        scanf("%d", &grades[i]);
+    }
+
+    // tells user matric number
+    printf("Results for student: %d\n", matricNumber);
+
+    // for loop through the grades and displays them
+    for (int i = 0; i < gradeNumber; ++i) {
         printf("%d \n", grades[i]);
     }
 
